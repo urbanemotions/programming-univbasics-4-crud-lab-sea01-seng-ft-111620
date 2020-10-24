52f95900dca3c1ea9bbbba5294ef00a3e6a7cc0e
Sin Wildy <sw.wildy@gmail.com>
2020-10-24 00:58:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,11 +10,11 @@ def add_element_to_end_of_array(array, element)
   #array << "arrays!"
   #array << element
   array.push(element)
-  p array
+  #p array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
