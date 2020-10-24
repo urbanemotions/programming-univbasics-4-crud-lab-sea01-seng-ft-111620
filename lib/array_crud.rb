4cb1ef1040ca7c02c6d6a6943e59d4b20d1a2517
Sin Wildy <sw.wildy@gmail.com>
2020-10-24 01:08:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,8 +19,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop
-  p array
+  last = array.pop
+  p last
 end
 
 def remove_element_from_start_of_array(array)
