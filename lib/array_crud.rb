8a3a5743644e46181d5b15d04473fd5b479fa150
Sin Wildy <sw.wildy@gmail.com>
2020-10-24 00:45:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  designer_brand = ["Louis Vuitton", "Loewe", "Miu Miu", "Tory Burch"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << "arrays"
 end
 
 def add_element_to_start_of_array(array, element)
